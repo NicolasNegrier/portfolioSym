c9316159c52b1d957a17ebab6fab797eb6276132
NicolasNegrier <nicolas.negrier@gmail.com>
2023-02-20 16:53:55 +0100

    mise en place du visuel avec decoupage des sections, mise en place de l'effet de switch des sectionséééé

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -132,11 +132,108 @@ const app = {
         }
     },
 
+    switchSection: () => {
+
+        // On recupere le conteneur de mes infos portfolio
+        const main = document.querySelector(".containerInfos");
+
+        // On récupère le conteneur des sections et le conteneur de projets
+        let sections = document.querySelector(".sections");
+        let projects = document.querySelector(".projects__container");
+
+        // On récupère un tableau contenant les differentes sections et projets
+        let sectionInfos = Array.from(sections.children);
+        let projectInfos = Array.from(projects.children);
+
+        // On récupère les deux flèches
+        const right = document.querySelector("#nav-right");
+        const left = document.querySelector("#nav-left");
+        const up = document.querySelector("#nav-up");
+        const down = document.querySelector("#nav-down");
+
+        // On calcule la largeur visible du diaporama
+        let mainWidth = main.getBoundingClientRect().width;
+        let mainHeight = main.getBoundingClientRect().height;
+
+        // Initialisation d'un compteur et 
+        let compteurX = 0;
+        let compteurY = 0;
+        let decal;
+
+        const navRight = () => {
+
+            // Incrementation du compteur
+            compteurX++;
+
+            if(compteurX == sectionInfos.length){
+                compteurX = 0;
+            }
+
+            decal = -mainWidth * compteurX;
+            sections.style.transform = `translateX(${decal}px)`;
+        }
+
+        const navLeft = () => {
+
+            // Incrementation du compteur
+            compteurX--;
+
+            if(compteurX == -1){
+                compteurX = (sectionInfos.length - 1);
+            }
+
+            decal = -mainWidth * compteurX;
+            sections.style.transform = `translateX(${decal}px)`;
+        }
+
+        const navDown = () => {
+
+            // Incrementation du compteur
+            compteurY++;
+
+            if(compteurY == projectInfos.length){
+                compteurY = 0;
+            }
+
+            decal = -mainHeight * compteurY;
+            projects.style.transform = `translateY(${decal}px)`;
+        }
+
+        const navUp = () => {
+
+            // Incrementation du compteur
+            compteurY--;
+
+            if(compteurY == -1){
+                compteurY = (projectInfos.length - 1);
+            }
+
+            decal = -mainHeight * compteurY;
+            projects.style.transform = `translateY(${decal}px)`;
+        }
+
+
+        // On met en place les écouteurs d'évènements sur les flèches
+        right.addEventListener("click", navRight);
+        left.addEventListener("click", navLeft);
+        down.addEventListener("click", navDown);
+        up.addEventListener("click", navUp);
+
+        const resize = () => {
+            mainWidth = main.getBoundingClientRect().width;
+        }
+
+        // Mise en oeuvre du "responsive"
+        window.onresize = resize;
+
+    },
+
     init: () => {
         app.headerAnimate();
         app.scrollListner();
         app.windowSeize();
         app.toggleMenu();
+        app.switchSection();
     },
 };
 
--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\TechnoCatRepository;
+use App\Repository\TechnoRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,12 +12,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class PortfolioController extends AbstractController
 {
     #[Route('/', name: 'portfolio_index')]
-    public function index(UserRepository $userRepository): Response
+    public function index(UserRepository $userRepository, TechnoCatRepository $technoCatRepository): Response
     {
         $user = $userRepository->find(2);
+        $technoCats = $technoCatRepository->findAll();
 
         return $this->render('portfolio/index.html.twig', [
             'user' => $user,
+            'technoCats' => $technoCats
         ]);
     }
 }
